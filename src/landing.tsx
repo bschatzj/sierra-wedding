50fa3936ce674a5796d5aafbb06d1257f2d0e408
Brendan Schatz <brendanschatz97@gmail.com>
2026-03-01 17:25:47 -0600

    Update landing.tsx

--- src/landing.tsx
+++ src/landing.tsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom";
 
 const Landing =  () => {
 
@@ -15,7 +16,7 @@ const Landing =  () => {
 <div id='quiz-section' className="p-10 space-y-8">
     <h1 className='text-sky-900 text-3xl underline text-center'> How to play</h1>
     <p className='text-gray-800 text-xl'>Daksh and Sierra wanted to have a little fun at the wedding and see how well their guests know them. They created a quiz of 15 questions to find out! Answer the questions and learn a bit more about their relationship as you go. Enter your email at the end and you could win a small prize if you got the most correct.</p>
-    <a href='/quiz/1'><button className='mt-4 border-1 border-aqua-200 py-2 w-full rounded-full text-contrast-text text-bold text-lg '>Lets go!</button></a>
+    <Link to="/quiz/1"><button className='mt-4 border-1 border-aqua-200 py-2 w-full rounded-full text-contrast-text text-bold text-lg '>Lets go!</button></Link>
 </div>
 </div>)}
 
