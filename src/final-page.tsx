4babf48e606dd947bbd06c4203a3afa0e9dce96b
Brendan Schatz <brendanschatz97@gmail.com>
2026-03-13 15:07:37 -0700

    Few more question tweaks and logic around submit change.

--- src/final-page.tsx
+++ src/final-page.tsx
@@ -1,81 +1,111 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import useLocalStorage from "./local-storage-hook.ts";
 
 const SubmitPage = () => {
-    const [trueCount, setTrueCount] = useState(0);
-    const [name, setName] = useState("");
-    const [tiebreaker, setTiebreaker] = useState("");
-    const [submitted, setSubmitted] = useLocalStorage("submitted", "false");
+  const [trueCount, setTrueCount] = useState(0);
+  const [name, setName] = useState("");
+  const [tiebreaker, setTiebreaker] = useState("");
+  const [submitted, setSubmitted] = useLocalStorage("submitted", "false");
+  const [error, setError] = useState(false);
 
-    useEffect(() => {
-        let count = 0;
+  useEffect(() => {
+    let count = 1;
 
-        for (let i = 0; i < localStorage.length; i++) {
-            const key = localStorage.key(i);
-            const value = localStorage.getItem(key as string);
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = localStorage.getItem(key as string);
 
-            if (value!.toString() === '"true"') {
-                count++;
-            }
-        }
-        // eslint-disable-next-line react-hooks/set-state-in-effect
-        return setTrueCount(count);
-    }, []);
+      if (value!.toString() === '"true"') {
+        count++;
+      }
+    }
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    return setTrueCount(count);
+  }, []);
 
-    const correct = trueCount;
-    const total = localStorage.length;
+  const total = localStorage.length;
 
-    const handleSubmit = async (e: { preventDefault: () => void; }) => {
-        e.preventDefault();
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    setError(false);
+    if (!name || !tiebreaker) {
+      setError(true);
+      return;
+    }
+    e.preventDefault();
 
-        const formURL =
-            "https://docs.google.com/forms/d/e/1FAIpQLSeHzBtzRneQc2GIFD0bMH2FbwL6ZlddhRWUvMjd2T1yAJ3ZeQ/formResponse";
+    const formURL =
+      "https://docs.google.com/forms/d/e/1FAIpQLSeHzBtzRneQc2GIFD0bMH2FbwL6ZlddhRWUvMjd2T1yAJ3ZeQ/formResponse";
 
-        const formData = new FormData();
-        formData.append("entry.1070326341", name);
-        formData.append("entry.26786542", tiebreaker);
-        formData.append("entry.77133912", trueCount.toString());
+    const formData = new FormData();
+    formData.append("entry.1070326341", name);
+    formData.append("entry.26786542", tiebreaker);
+    formData.append("entry.77133912", trueCount.toString());
 
-        await fetch(formURL, {
-            method: "POST",
-            mode: "no-cors",
-            body: formData,
-        });
+    await fetch(formURL, {
+      method: "POST",
+      mode: "no-cors",
+      body: formData,
+    });
 
-        setSubmitted("true");
-    };
+    setSubmitted("true");
+  };
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-emerald-400 to-green-200 p-6">
-            <div className="bg-white rounded-3xl shadow-xl p-10 max-w-lg w-full text-center space-y-6">
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-emerald-400 to-green-200 p-6">
+      <div className="bg-white rounded-3xl shadow-xl p-10 max-w-lg w-full text-center space-y-6">
+        <h1 className="text-4xl font-bold text-gray-800">Crushed it!</h1>
 
-                <h1 className="text-4xl font-bold text-gray-800">
-                    Crushed it!
-                </h1>
+        <p className="text-gray-500 text-lg">
+          {trueCount} / {total} correct
+        </p>
+        {submitted !== "true" ? (
+          <>
+            {" "}
+            <p className="text-xl font-bold text-gray-800">Tie Breaker!!!</p>
+            <p>
+              If you add the number of letters in Daksh and Sierra's middle
+              names. Multiply that by the distance in miles between their first
+              address together and their current address. Divide that number by
+              the total years Daksh and Sierra were in college. Closest to
+              correct number will win the tie breaker!
+            </p>
+            {error && (
+              <p className="text-red-400">
+                Please add both name and a tiebreaker guess!
+              </p>
+            )}
+            <input
+              placeholder="Player name"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              className="p-1 border-1 rounded-lg w-full"
+            />
+            <input
+              placeholder="Guess"
+              onChange={(e) => {
+                setTiebreaker(e.target.value);
+              }}
+              className="p-1 border-1 rounded-lg w-full"
+              type="number"
+            />
+            <button
+              className="mt-4 border-1 border-aqua-200 py-2 w-full rounded-full text-contrast-text text-bold text-lg "
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </>
+        ) : (
+          <p className="text-gray-500 text-lg">
+            We'll let you know how you did!
+          </p>
+        )}
 
-                <p className="text-gray-500 text-lg">
-                    {correct} / {total} correct
-                </p>
-                {submitted !== 'true' ? <> <p className="text-xl font-bold text-gray-800">
-                    Tie Breaker!!!
-                </p>
-                <p>
-                    If you add the number of letters in Daksh and Sierra's middle names. Multiply that by the distance in miles between their first address together and their current address. Divide that number by the total years Daksh and Sierra were in college. Closest to correct number will win the tie breaker!
-                </p>
-                <input placeholder='Player name' onChange={(e) => {setName(e.target.value)}} className='p-1 border-1 rounded-lg w-full' />
-                <input placeholder='Guess' onChange={(e) => {setTiebreaker(e.target.value)}} className='p-1 border-1 rounded-lg w-full' type='number'/>
-                <button className='mt-4 border-1 border-aqua-200 py-2 w-full rounded-full text-contrast-text text-bold text-lg ' onClick={handleSubmit} >Submit</button>
-                </> : <p className="text-gray-500 text-lg">We'll let you know how you did!</p>}
+        <div className="text-3xl animate-bounce space-x-2">🎊 🧖‍♀️ 🧖‍♂️ 🎊</div>
+      </div>
+    </div>
+  );
+};
 
-            <div className="text-3xl animate-bounce space-x-2">
-                     🎊 🧖‍♀️ 🧖‍♂️ 🎊
-                </div>
-
-            </div>
-        </div>
-    );
-
-}
-
-
-export default SubmitPage
+export default SubmitPage;
--- src/question-list.ts
+++ src/question-list.ts
@@ -1,200 +1,228 @@
-import Dog from "./assets/bog2.jpg"
-import Ski from "./assets/SkiToSea.jpg"
-import Wrist from "./assets/wrist.jpg"
-import YMCA from "./assets/ymca.jpg"
+import Dog from "./assets/bog2.jpg";
+import Ski from "./assets/SkiToSea.jpg";
+import Wrist from "./assets/wrist.jpg";
+import YMCA from "./assets/ymca.jpg";
 export const QuestionList = [
+  {
+    question: "How did Sierra and Daksh meet?",
+    options: [
+      "Dating app",
+      "Rock climbing gym",
+      "Mutual friend's birthday",
+      "Roommate wanted post on Facebook",
+    ],
+    answer: "Mutual friend's birthday",
+    questionId: 1,
+    imageUrl:
+      "https://images.zola.com/5ab273ef-8b6c-4121-8f12-375c4837794a?w=1000",
+    explanation: "June 2022 - dancing at Chop Suey",
+  },
+  {
+    question:
+      "What was Daksh and Sierra's halloween couples costume their first year of dating?",
+    options: [
+      "Peter Pan and Tinkerbell",
+      "Shrek and Fiona",
+      "Wall-E and Eve",
+      "Prince Charming and Cinderella",
+    ],
+    answer: "Wall-E and Eve",
+    questionId: 2,
+    imageUrl:
+      "https://images.zola.com/cc1d8905-7e7e-4d43-b445-7fd02d96e607?w=1000",
+    explanation: "Daksh was Wall-E and Sierra was Eve!",
+  },
+  {
+    question: "Where did Sierra and Daksh go on their first date?",
+    options: [
+      "Ethiopian restaurant",
+      "Kenyan restaurant",
+      "Nepalese restaurant",
+      "Vietnamese restaurant",
+    ],
+    answer: "Kenyan restaurant",
+    questionId: 3,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/kenyan-food-delicious-meals-cuisines-yummy-sweet-beverages-dinner-dessert-lunch-supper-nairobi-city_257688-4046.jpg?w=2000",
+    explanation: "",
+  },
+  {
+    question:
+      "Where was Sierra and Daksh's first international travel destination?",
+    options: [
+      "Toronto, Canada",
+      "Mexico City, Mexico",
+      "Vancouver, Canada",
+      "San Juan, Puerto Rico",
+    ],
+    answer: "Vancouver, Canada",
+    questionId: 4,
+    imageUrl:
+      "https://th.bing.com/th/id/R.c52e90008d9a8a3cdffa5d00a9ba14f4?rik=0vIC4lxTnW29gA&pid=ImgRaw&r=0",
+    explanation:
+      "Daksh and Sierra went up to Vancouver and went skiing at Whistler for a few days.",
+  },
 
-    {
-        question: "How did Sierra and Daksh meet?",
-        options: ["Dating app", "Rock climbing gym", "Mutual friend's birthday", "Roommate wanted post on Facebook"],
-        answer: "Mutual friend's birthday",
-        questionId: 1,
-        imageUrl: "https://images.zola.com/5ab273ef-8b6c-4121-8f12-375c4837794a?w=1000",
-        explanation: "June 2022 - dancing at Chop Suey"
-    },
-    {
-        question: "What was Daksh and Sierra's halloween couples costume their first year of dating?",
-        options: ["Peter Pan and Tinkerbell", "Shrek and Fiona", "Wall-E and Eve", "Prince Charming and Cinderella"],
-        answer: "Wall-E and Eve",
-        questionId: 2,
-        imageUrl: "https://images.zola.com/cc1d8905-7e7e-4d43-b445-7fd02d96e607?w=1000",
-        explanation: "Daksh was Wall-E and Sierra was Eve!"
-    },
-    {
-        question: "Where did Sierra and Daksh go on their first date?",
-        options: ["Ethiopian restaurant", "Kenyan restaurant", "Nepalese restaurant", "Vietnamese restaurant"],
-        answer: "Kenyan restaurant",
-        questionId: 3,
-        imageUrl: "https://img.freepik.com/premium-photo/kenyan-food-delicious-meals-cuisines-yummy-sweet-beverages-dinner-dessert-lunch-supper-nairobi-city_257688-4046.jpg?w=2000",
-        explanation: ""
-    },
-    {
-        question: "Where was Sierra and Daksh's first international travel destination?",
-        options: ["Toronto, Canada", "Mexico City, Mexico", "Vancouver, Canada", "San Juan, Puerto Rico"],
-        answer: "Vancouver, Canada",
-        questionId: 4,
-        imageUrl: "https://th.bing.com/th/id/R.c52e90008d9a8a3cdffa5d00a9ba14f4?rik=0vIC4lxTnW29gA&pid=ImgRaw&r=0",
-        explanation: ""
-    },
+  {
+    question:
+      "What type of robot was Daksh's primary focus of study during his PHD?",
+    options: ["Roombas", "Flying Insect", "Humanoid", "Drones"],
+    answer: "Flying Insect",
+    questionId: 5,
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.BbVCz-UTElBLmhnly9dHwQHaEJ?w=333&h=187&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
+    explanation:
+      "Sierra went and watched Daksh defend his PHD thesis on flying insect robots at University of Washington.",
+  },
 
-    {
-        question: "What type of robot was Daksh's primary focus of study during his PHD?",
-        options: ["Roombas", "Flying Insect", "Humanoid", "Drones"],
-        answer: "Flying Insect",
-        questionId: 5,
-        imageUrl: "https://th.bing.com/th/id/OIP.BbVCz-UTElBLmhnly9dHwQHaEJ?w=333&h=187&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
-        explanation: "Sierra went and watched Daksh defend his PHD thesis on flying insect robots at University of Washington."
-    },
+  {
+    question:
+      "What two events did Sierra and Daksh compete in, in last year's Ski to Sea race?",
+    options: [
+      "Cross Country Skiing and Running",
+      "Downhill skiing and Road Biking",
+      "Road Biking and Canoeing",
+      "Their team had to forfeit",
+    ],
+    answer: "Road Biking and Canoeing",
+    questionId: 6,
+    imageUrl: Ski,
+    explanation:
+      "Daksh competed in the road biking portion and Sierra competed in the canoe.",
+  },
 
-
-    {
-        question: "What two events did Sierra and Daksh compete in, in last year's Ski to Sea race?",
-        options: ["Cross Country Skiing and Running", "Downhill skiing and Road Biking", "Road Biking and Canoeing", "Their team had to forfeit"],
-        answer: "Road Biking and Canoeing",
-        questionId: 6,
-        imageUrl: Ski,
-        explanation: "Daksh competed in the road biking portion and Sierra competed in the canoe."
-    },
-
-
-    {
-        question: "How did Daksh tear his ACL?",
-        options: ["Running with Korra", "Snowboarding", "Skiing", "Playing Volleyball"],
-        answer: "Snowboarding",
-        questionId: 7,
-        imageUrl: "https://wallpapers.com/images/hd/extreme-sports-snowboarding-jump-hxi9sfpdpvl8w3of.jpg",
-        explanation: "*not actual photo of Daksh snowboarding*"
-    },
-    {
-        question: "How did Sierra break her wrist in Panama?",
-        options: [
-            "Sliding down a dusty hill",
-            "Running away from a vicious turkey and slipped",
-            "Fell into a water tank hole",
-            "Playing with some children in her community"
-        ],
-        answer: "Sliding down a dusty hill",
-        questionId: 8,
-        imageUrl: Wrist,
-        explanation: ""
-    },
-    {
-        question: "How are Sierra and Daksh most likely spending their Saturday?",
-        options: [
-            "Going out for breakfast, museum and a movie",
-            "Hitting the library, organizing the house and ordering take out",
-            "Getting away hiking, climbing, or going to a concert with friends",
-            "Sleeping in late and catching up on TV shows"
-        ],
-        answer: "Getting away hiking, climbing, or going to a concert with friends",
-        questionId: 9,
-        imageUrl: "https://images.zola.com/ab0eac20-94e1-4032-86d7-d49efe231027?w=1000",
-        explanation: "Any chance to be in nature just the two of them (and korra of course)  or with friends is their ideal way to spend a Saturday!"
-    },
-    {
-        question: "At whose birthday party did Daksh and Sierra first meet?",
-        options: [
-            "Sam",
-            "Alice",
-            "Roshni",
-            "Caleb"
-        ],
-        answer: "Roshni",
-        questionId: 10,
-        imageUrl: "https://wallpapers.com/images/hd/birthday-balloons-pictures-e92r9493bqmm79il.jpg",
-        explanation: ""
-    },
-    {
-        question: "Who said I love you first?",
-        options: [
-            "Sierra",
-            "Daksh",
-            "Sierra, prompted by Daksh",
-            "Daksh, prompted by Sierra"
-        ],
-        answer: "Daksh, prompted by Sierra",
-        questionId: 11,
-        imageUrl: "https://images.zola.com/50d9f334-4dae-466f-b4f6-d99ca25de620?w=1000",
-        explanation: ""
-    },
-    {
-        question: "What food or drink did Sierra and Daksh have on their first international trip together and commemorated with a selfie shared on their wedding website? No cheating!",
-        options: [
-            "Tim Horton's coffee",
-            "Timbits",
-            "Nanaimo bars",
-            "Poutine"
-        ],
-        answer: "Tim Horton's coffee",
-        questionId: 12,
-        imageUrl: "https://images.zola.com/82a2c724-df8a-4bfc-bae7-c93b62476cf9?w=1000",
-        explanation: ""
-    },
-    {
-        question: "How many combined round trip flights did Sierra and Daksh take to visit each other in 2025?",
-        options: [
-            "13",
-            "17",
-            "23",
-            "27"
-        ],
-        answer: "27",
-        questionId: 13,
-        imageUrl: "https://www.rd.com/wp-content/uploads/2022/04/GettyImages-1140602972-e1651249657746.jpg",
-        explanation: "Nearly 50,000 miles traveled in those flights alone!"
-    },
-    {
-        question: "What protein does Daksh most often cook for Sierra?",
-        options: [
-            "Tofu",
-            "Chicken",
-            "Salmon",
-            "Lentils"
-        ],
-        answer: "Salmon",
-        questionId: 14,
-        imageUrl: "https://www.foodiecrush.com/wp-content/uploads/2019/05/Grilled-Salmon-foodiecrush.com-023-1024x1536.jpg",
-        explanation: "Daksh is a phenomenal cook and can make a wide variety of meals, but salmon is his go-to for Sierra!"
-    },
-    {
-        question: "What are Sierra and Daksh's preferred methods to make their coffee?",
-        options: [
-            "Daksh: espresso machine; Sierra: French press",
-            "Daksh: pour over; Sierra: Moka pot",
-            "Daksh: pods; Sierra: drip",
-            "Daksh: aero press; Sierra: siphon pot"
-        ],
-        answer: "Daksh: espresso machine; Sierra: French press",
-        questionId: 15,
-        imageUrl: "https://madtasting.com/wp-content/uploads/2024/12/top_coffee_espresso_machines.jpg",
-        explanation: ""
-    },
-    {
-        question: "Which university did Daksh attend for undergrad?",
-        options: [
-            "Delhi College of Engineering",
-            "University of Washington",
-            "Indian Institute of Technology - Delhi",
-            "YMCA University"
-        ],
-        answer: "YMCA University",
-        questionId: 16,
-        imageUrl: YMCA,
-        explanation: ""
-    },
-    {
-        question: "Which one is Korra?",
-        options: [
-            "a",
-            "b",
-            "c",
-            "d"
-        ],
-        type: "image",
-        answer: "b",
-        questionId: 17,
-        imageUrl: Dog,
-        explanation: "It's Korra!!! "
-    }
-
-]
+  {
+    question: "How did Daksh tear his ACL?",
+    options: [
+      "Running with Korra",
+      "Snowboarding",
+      "Skiing",
+      "Playing Volleyball",
+    ],
+    answer: "Snowboarding",
+    questionId: 7,
+    imageUrl:
+      "https://wallpapers.com/images/hd/extreme-sports-snowboarding-jump-hxi9sfpdpvl8w3of.jpg",
+    explanation: "*not actual photo of Daksh snowboarding*",
+  },
+  {
+    question: "How did Sierra break her wrist in Panama?",
+    options: [
+      "Sliding down a dusty hill",
+      "Running away from a vicious turkey and slipped",
+      "Fell into a water tank hole",
+      "Playing with some children in her community",
+    ],
+    answer: "Sliding down a dusty hill",
+    questionId: 8,
+    imageUrl: Wrist,
+    explanation:
+      "After falling Sierra had to hike over two miles, then take a bus to finally get help for her wrist.",
+  },
+  {
+    question: "How are Sierra and Daksh most likely spending their Saturday?",
+    options: [
+      "Going out for breakfast, museum and a movie",
+      "Hitting the library, organizing the house and ordering take out",
+      "Getting away hiking, climbing, or going to a concert with friends",
+      "Sleeping in late and catching up on TV shows",
+    ],
+    answer: "Getting away hiking, climbing, or going to a concert with friends",
+    questionId: 9,
+    imageUrl:
+      "https://images.zola.com/ab0eac20-94e1-4032-86d7-d49efe231027?w=1000",
+    explanation:
+      "Any chance to be in nature just the two of them (and korra of course)  or with friends is their ideal way to spend a Saturday!",
+  },
+  {
+    question: "At whose birthday party did Daksh and Sierra first meet?",
+    options: ["Sam", "Alice", "Roshni", "Caleb"],
+    answer: "Roshni",
+    questionId: 10,
+    imageUrl:
+      "https://wallpapers.com/images/hd/birthday-balloons-pictures-e92r9493bqmm79il.jpg",
+    explanation: "",
+  },
+  {
+    question: "Who said I love you first?",
+    options: [
+      "Sierra",
+      "Daksh",
+      "Sierra, prompted by Daksh",
+      "Daksh, prompted by Sierra",
+    ],
+    answer: "Daksh, prompted by Sierra",
+    questionId: 11,
+    imageUrl:
+      "https://images.zola.com/50d9f334-4dae-466f-b4f6-d99ca25de620?w=1000",
+    explanation: "",
+  },
+  {
+    question:
+      "What food or drink did Sierra and Daksh have on their first international trip together and commemorated with a selfie shared on their wedding website? No cheating!",
+    options: ["Tim Horton's coffee", "Timbits", "Nanaimo bars", "Poutine"],
+    answer: "Tim Horton's coffee",
+    questionId: 12,
+    imageUrl:
+      "https://images.zola.com/82a2c724-df8a-4bfc-bae7-c93b62476cf9?w=1000",
+    explanation: "",
+  },
+  {
+    question:
+      "How many combined round trip flights did Sierra and Daksh take to visit each other in 2025?",
+    options: ["13", "17", "23", "27"],
+    answer: "27",
+    questionId: 13,
+    imageUrl:
+      "https://www.rd.com/wp-content/uploads/2022/04/GettyImages-1140602972-e1651249657746.jpg",
+    explanation: "Nearly 50,000 miles traveled in those flights alone!",
+  },
+  {
+    question: "What protein does Daksh most often cook for Sierra?",
+    options: ["Tofu", "Chicken", "Salmon", "Lentils"],
+    answer: "Salmon",
+    questionId: 14,
+    imageUrl:
+      "https://www.foodiecrush.com/wp-content/uploads/2019/05/Grilled-Salmon-foodiecrush.com-023-1024x1536.jpg",
+    explanation:
+      "Daksh is a phenomenal cook and can make a wide variety of meals, but salmon is his go-to for Sierra!",
+  },
+  {
+    question:
+      "What are Sierra and Daksh's preferred methods to make their coffee?",
+    options: [
+      "Daksh: espresso machine; Sierra: French press",
+      "Daksh: pour over; Sierra: Moka pot",
+      "Daksh: pods; Sierra: drip",
+      "Daksh: aero press; Sierra: siphon pot",
+    ],
+    answer: "Daksh: espresso machine; Sierra: French press",
+    questionId: 15,
+    imageUrl:
+      "https://madtasting.com/wp-content/uploads/2024/12/top_coffee_espresso_machines.jpg",
+    explanation: "",
+  },
+  {
+    question: "Which university did Daksh attend for undergrad?",
+    options: [
+      "Delhi College of Engineering",
+      "University of Washington",
+      "Indian Institute of Technology - Delhi",
+      "YMCA University",
+    ],
+    answer: "YMCA University",
+    questionId: 16,
+    imageUrl: YMCA,
+    explanation:
+      "Originally an Indo-German project from 1969, it was converted to a university in 2009.",
+  },
+  {
+    question: "Which one is Korra?",
+    options: ["a", "b", "c", "d"],
+    type: "image",
+    answer: "b",
+    questionId: 17,
+    imageUrl: Dog,
+    explanation: "It's Korra!!! ",
+  },
+];
